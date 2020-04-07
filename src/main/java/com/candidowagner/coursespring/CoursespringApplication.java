16a4e90b06ac3847b56dc3170ac3d38339a5beb5
Wagner <wagner.silvac@ctis.com.br>
2020-04-07 19:17:54 -0300

    Primeiro commit

---
+++ src/main/java/com/candidowagner/coursespring/CoursespringApplication.java
@@ -0,0 +1,13 @@
+package com.candidowagner.coursespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringApplication.class, args);
+	}
+
+}

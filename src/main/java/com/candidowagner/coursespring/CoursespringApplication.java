e9ac79afc52190534de3d85cc5cc813bee1b5473
Wagner <wagner.silvac@ctis.com.br>
2020-04-07 21:57:32 -0300

    Adicionando Produto e Associação de muitos para muitos

--- src/main/java/com/candidowagner/coursespring/CoursespringApplication.java
+++ src/main/java/com/candidowagner/coursespring/CoursespringApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.candidowagner.coursespring.domain.Categoria;
+import com.candidowagner.coursespring.domain.Produto;
 import com.candidowagner.coursespring.repositories.CategoriaRepository;
+import com.candidowagner.coursespring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CoursespringApplication implements CommandLineRunner {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursespringApplication.class, args);
@@ -24,8 +29,20 @@ public class CoursespringApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		Produto prod1 = new Produto(null, "Computador", 2000.00);
+		Produto prod2 = new Produto(null, "Impressora", 800.00);
+		Produto prod3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 	}
 
 }

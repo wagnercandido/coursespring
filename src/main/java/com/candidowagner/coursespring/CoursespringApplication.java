5eea802cafbfb549adbb9848d7f5d0efa4e249ce
Wagner <wagner.silvac@ctis.com.br>
2020-04-07 21:30:29 -0300

    Criando instaciação da Base

--- src/main/java/com/candidowagner/coursespring/CoursespringApplication.java
+++ src/main/java/com/candidowagner/coursespring/CoursespringApplication.java
@@ -1,13 +1,31 @@
 package com.candidowagner.coursespring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.candidowagner.coursespring.domain.Categoria;
+import com.candidowagner.coursespring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CoursespringApplication {
+public class CoursespringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursespringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }

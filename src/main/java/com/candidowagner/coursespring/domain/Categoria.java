82f7c84ae042e06fc61c88dcad94a46c0675ccc7
Wagner <wagner.silvac@ctis.com.br>
2020-04-07 22:20:21 -0300

    Tratamento de exceção no endpoint de categorias/id

--- src/main/java/com/candidowagner/coursespring/domain/Categoria.java
+++ src/main/java/com/candidowagner/coursespring/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 
@@ -20,6 +22,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 
+	@JsonManagedReference  // TODO Anotação utilizada na classe onde deve exibir a lista de objetos
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 

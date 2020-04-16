0ec7257eb19ad1de92daf79c854292ed88f2b3ad
Wagner <wagner.silvac@ctis.com.br>
2020-04-16 14:57:33 -0300

    POST Cliente

--- src/main/java/com/candidowagner/coursespring/services/ClienteService.java
+++ src/main/java/com/candidowagner/coursespring/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.candidowagner.coursespring.domain.Cidade;
 import com.candidowagner.coursespring.domain.Cliente;
+import com.candidowagner.coursespring.domain.Endereco;
+import com.candidowagner.coursespring.domain.enums.TipoCliente;
 import com.candidowagner.coursespring.dto.ClienteDTO;
+import com.candidowagner.coursespring.dto.ClienteFullDTO;
 import com.candidowagner.coursespring.repositories.ClienteRepository;
+import com.candidowagner.coursespring.repositories.EnderecoRepository;
 import com.candidowagner.coursespring.services.exceptions.DataIntegrityException;
 import com.candidowagner.coursespring.services.exceptions.ObjectNotFoundException;
 
@@ -22,16 +28,22 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente getByID(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
-//	public Cliente save(Cliente obj) {
-//		obj.setId(null);
-//		return repository.save(obj);
-//	}
+	@Transactional
+	public Cliente save(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 
 	public Cliente update(Cliente obj) {
 		Cliente newObj = getByID(obj.getId());
@@ -57,13 +69,31 @@ public class ClienteService {
 		return repository.findAll(pageRequest);
 	}
 
-	public Cliente fromDTO(ClienteDTO objDto) {
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
-	}
-	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getNome());
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteFullDTO objDto) {
+		Cliente cliente = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getDocumento(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cidade = new Cidade(objDto.getCidadeID(), null, null);
+		Endereco endereco = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cliente, cidade);
+		cliente.getEnderecos().add(endereco);
+		cliente.getTelefones().add(objDto.getTelefone1());
+
+		if (objDto.getTelefone2() != null) {
+			cliente.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cliente.getTelefones().add(objDto.getTelefone3());
+		}
+		return cliente;
 	}
 
 }

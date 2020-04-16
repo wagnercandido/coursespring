49333efcb8ef743fae62197177eb9d52a86dd409
Wagner <wagner.silvac@ctis.com.br>
2020-04-15 23:36:41 -0300

    GET all Categoria com CategoriaDTO

--- src/main/java/com/candidowagner/coursespring/resources/CategoriaResource.java
+++ src/main/java/com/candidowagner/coursespring/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.candidowagner.coursespring.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.candidowagner.coursespring.domain.Categoria;
+import com.candidowagner.coursespring.dto.CategoriaDTO;
 import com.candidowagner.coursespring.services.CategoriaService;
 
 @RestController
@@ -45,6 +48,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> remover(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> listar() {
+		List<Categoria> lista = service.getAll();
+		List<CategoriaDTO> retorno = lista.stream().map(item -> new CategoriaDTO(item)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(retorno);
 	}
 
 }

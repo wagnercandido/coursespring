99e04782867656c6bda7a5cd05792246d4e35a26
Wagner <wagner.silvac@ctis.com.br>
2020-04-20 19:42:34 -0300

    Implementando Authorização

--- src/main/java/com/candidowagner/coursespring/config/SerucityConfig.java
+++ src/main/java/com/candidowagner/coursespring/config/SerucityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.candidowagner.coursespring.security.JWTAuthenticationFilter;
+import com.candidowagner.coursespring.security.JWTAuthorizationFilter;
 import com.candidowagner.coursespring.security.JWTUtil;
 
 @Configuration
@@ -51,6 +52,7 @@ public class SerucityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_ROUTES_GET)
 			.permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 

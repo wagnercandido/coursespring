4f2f980f6e40d41bf64965009aba8f5018b7a76e
Wagner <wagner.silvac@ctis.com.br>
2020-04-22 14:18:45 -0300

    Tratamento de Exceções e upload

--- src/main/java/com/candidowagner/coursespring/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/candidowagner/coursespring/resources/exception/ResourceExceptionHandler.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.candidowagner.coursespring.services.exceptions.AuthorizationException;
 import com.candidowagner.coursespring.services.exceptions.DataIntegrityException;
+import com.candidowagner.coursespring.services.exceptions.FileException;
 import com.candidowagner.coursespring.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -44,6 +48,35 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
 		StandardError error = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		
+	}
+	
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> authorization(FileException e, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		
+	}
+	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.valueOf(e.getErrorCode());
+		StandardError error = new StandardError(status.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(status).body(error);
+		
+	}
+	
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		
+	}
+	
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardError> amazonS3(AmazonS3Exception e, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 		
 	}
 
--- src/main/java/com/candidowagner/coursespring/services/S3Service.java
+++ src/main/java/com/candidowagner/coursespring/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.candidowagner.coursespring.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -33,7 +34,7 @@ public class S3Service {
 			String contentType = multiPartFile.getContentType();
 			return uploadFile(fileName, inputStream, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("AmazonClientException" + e.getMessage());
+			throw new FileException("AmazonClientException" + e.getMessage());
 		}
 
 	}
@@ -47,7 +48,7 @@ public class S3Service {
 			LOG.info("Upload Realizado com Sucesso!");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL to URI");
+			throw new FileException("Erro ao converter URL to URI");
 		}
 	}
 
